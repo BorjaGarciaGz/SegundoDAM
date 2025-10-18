5c9b8225253d7cccc6ff8e04cccfbcf9c81d636e
Borja García Gómez <borjagarciagz82@gmail.com>
2025-10-18 20:13:41 +0200

    modificado

--- PSP/PspRepaso/src/main/java/nivel2/escribefecha/Padre.java
+++ PSP/PspRepaso/src/main/java/nivel2/escribefecha/Padre.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Padre {
